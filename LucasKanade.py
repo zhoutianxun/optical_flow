54e97a26e08606b5175ecd6dd912205a0365f544
zhoutianxun <zhoutianxun@gmail.com>
2021-02-17 14:56:52 +0800

    Add files via upload

--- LucasKanade.py
+++ LucasKanade.py
@@ -1,10 +1,10 @@
 import numpy as np
+from math import ceil
 import cv2
 import matplotlib.pyplot as plt
 
 class LucasKanadeObj:
-    def __init__(self, winsize=(15,15), maxLevel=2, 
-                 maxCorners=70, qualityLevel=0.3, minDistance=7, blockSize=7):
+    def __init__(self, winsize=(11,11), maxLevel=2):
         """
         Lucas Kanade object 
 
@@ -24,11 +24,11 @@ class LucasKanadeObj:
             average block size used for computing corner. The default is 8.
 
         """
-        self.feature_params = dict(maxCorners = maxCorners, qualityLevel = qualityLevel, minDistance = minDistance, blockSize = blockSize)
         self.lk_params = dict( winSize  = winsize, maxLevel = maxLevel, criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
         self.cap = None
         self.corners = None
         self.tracking_points = None
+        self.first_frame = None
     
     def load_video(self, path):
         # loads video
@@ -44,38 +44,55 @@ class LucasKanadeObj:
             print("Video is not loaded successfully, please check path, program exits now...")
             return False
     
-    def get_corners(self):
+    def display_first_frame(self):
+        # Helper function to display first frame
+        if type(self.first_frame) != list:
+            ret, frame = self.cap.read()
+            if ret:
+                plt.imshow(frame, cmap='gray')
+                plt.show(block=False)
+        else:
+            plt.imshow(self.first_frame, cmap='gray')
+            plt.show(block=False)
+        
+    def get_corners(self, maxCorners=70, qualityLevel=0.3, minDistance=7, blockSize=7):
         # Take first frame and find corners in it
+        self.corners = None
+        self.feature_params = dict(maxCorners = maxCorners, qualityLevel = qualityLevel, minDistance = minDistance, blockSize = blockSize)
         ret, old_frame = self.cap.read()
         old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
         self.corners = cv2.goodFeaturesToTrack(old_gray, mask = None, **self.feature_params)
         self.first_frame = old_frame
         
-    def user_choose_corners(self, feature_grid_size):
+    def display_corners(self, feature_grid_size):
         """
         feature_grid_size : int
             divide the video frame into grids of feature_grid_size, only 1 feature will appear in each grid.
         """
         
         # Plot the first image, and annotate the points that were selected as good features for tracking
+        plt.close()
         fig = plt.figure()
         fig.set_size_inches(15,20)
         plt.imshow(self.first_frame, cmap='gray')
 
         # Annotate points if there exist within n x n pixel grid
-        grid = np.zeros((self.first_frame.shape[0]//feature_grid_size, self.first_frame.shape[1]//feature_grid_size))
+        grid = np.zeros((ceil(self.first_frame.shape[0]/feature_grid_size), 
+                         ceil(self.first_frame.shape[1]//feature_grid_size)))
         counter = 0
         for point in self.corners:
             # check if another point has already been plotted in the same grid
             if grid[int(point[0,1]//feature_grid_size), int(point[0,0]//feature_grid_size)] == 0:
                 plt.scatter(point[0,0], point[0,1], s=4, c='red')
-                plt.annotate(counter, (point[0,0], point[0,1]), size=15, c='red')
+                plt.annotate(counter, (point[0,0], point[0,1]), size=8, c='red')
                 grid[int(point[0,1]//feature_grid_size), int(point[0,0]//feature_grid_size)] = 1.0
             counter += 1
         
         plt.show(block=False)
 
+    def user_choose_corners(self):
         # Allow user to choose the points to follow
+        self.tracking_points = None
         selections = input("Please key in the points you want to track, separated by comma (e.g. >> 3, 5,7 ): ")
         
         while True:
--- main.py
+++ main.py
@@ -5,13 +5,26 @@ lk_obj = LK.LucasKanadeObj()
 # Load video
 path = input("Please enter the name of your file, ensure that file is placed in same folder as this script: ")
 if lk_obj.load_video(path):
-
-	lk_obj.get_corners()
+    
 	# Let user choose points to track
-	lk_obj.user_choose_corners(50)
+    lk_obj.display_first_frame()
+    
+    while True:
+        try:
+            n = input("Roughly how many objects are present in the frame? Enter an integer: ")
+            n = int(n)
+            lk_obj.get_corners(maxCorners=2*n) 
+            lk_obj.display_corners(round(max(lk_obj.first_frame.shape[0]/n, lk_obj.first_frame.shape[1]/n)))
 
-	print("press esc to quit")
-	lk_obj.run()
+            if input("Enter y to proceed if the points you wish to track are marked out; Enter any other keys to choose number of objects again> ") in ['y' or 'Y']:
+                break 
+        except ValueError:
+            print("Please try again, enter an integer")
+
+    lk_obj.user_choose_corners()
+    
+    print("press esc to quit")
+    lk_obj.run()
 
 
 
