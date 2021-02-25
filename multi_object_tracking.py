96e83ae5e3f098093caca0a2ab8f3049e4a6dbd3
zhoutianxun <zhoutianxun@gmail.com>
2021-02-25 22:40:02 +0800

    Add files via upload

---
+++ multi_object_tracking.py
@@ -0,0 +1,147 @@
+import cv2
+import sys
+import matplotlib.pyplot as plt
+from random import randint
+
+(major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+
+trackerTypes = ['MIL', 'KCF', 'GOTURN', 'CSRT']
+
+def createTrackerByName(trackerType):
+  # Create a tracker based on tracker name
+  if trackerType == trackerTypes[0]:
+    tracker = cv2.legacy.TrackerMIL_create()
+  elif trackerType == trackerTypes[1]: 
+    tracker = cv2.legacy.TrackerKCF_create()
+  elif trackerType == trackerTypes[2]:
+    tracker = cv2.legacy.TrackerGOTURN_create()
+  elif trackerType == trackerTypes[3]:
+    tracker = cv2.legacy.TrackerCSRT_create()
+  else:
+    tracker = None
+    print('Incorrect tracker name')
+    print('Available trackers are:')
+    for t in trackerTypes:
+      print(t)
+    
+  return tracker
+
+# Read video
+path = input("Please enter name of file, ensure that file is in same folder: ")
+video = cv2.VideoCapture(path)
+
+# Exit if video not opened.
+if not video.isOpened():
+    print("Could not open video")
+    sys.exit()
+
+# Read first frame.
+ok, frame = video.read()
+if not ok:
+    print('Cannot read video file')
+    sys.exit()
+
+## Select boxes
+bboxes = []
+colors = [] 
+frame_copy = frame.copy()
+# OpenCV's selectROI function doesn't work for selecting multiple objects in Python
+# So we will call this function in a loop till we are done selecting all objects
+while True:
+  # draw bounding boxes over objects
+  # selectROI's default behaviour is to draw box starting from the center
+  # when fromCenter is set to false, you can draw box starting from top left corner
+  bbox = cv2.selectROI('MultiTracker', frame_copy, showCrosshair=True)
+  bboxes.append(bbox)
+  colors.append((randint(0, 255), randint(0, 255), randint(0, 255)))
+  
+  p1 = (int(bbox[0]), int(bbox[1]))
+  p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+  cv2.rectangle(frame_copy, p1, p2, colors[-1], 2, 1)
+  
+  print("Press q to quit selecting boxes and start tracking")
+  print("Press any other key to select next object")
+  k = cv2.waitKey(0) & 0xFF
+  if (k == 113):  # q is pressed
+    break
+
+print('Selected bounding boxes {}'.format(bboxes))
+
+# Specify the tracker type
+tracker_types = ['MIL','KCF', 'GOTURN', 'CSRT']
+print(f"Select tracker type by index: {tracker_types}")
+i = int(input(">> "))
+trackerType = tracker_types[i]
+
+# Create MultiTracker object
+multiTracker = cv2.legacy.MultiTracker_create()
+
+# Initialize MultiTracker 
+for bbox in bboxes:
+  multiTracker.add(createTrackerByName(trackerType), frame, bbox)
+
+"""
+# Initialize tracker with first frame and bounding box
+ok = tracker.init(frame, bbox)
+
+while True:
+    # Read a new frame
+    ok, frame = video.read()
+    if not ok:
+        break
+    
+    # Start timer
+    timer = cv2.getTickCount()
+
+    # Update tracker
+    ok, bbox = tracker.update(frame)
+
+    # Calculate Frames per second (FPS)
+    fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+
+    # Draw bounding box
+    if ok:
+        # Tracking success
+        p1 = (int(bbox[0]), int(bbox[1]))
+        p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+        cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+    else :
+        # Tracking failure
+        cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+
+    # Display tracker type on frame
+    cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2);
+
+    # Display FPS on frame
+    cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
+
+    # Display result
+    cv2.imshow("Tracking", frame)
+
+    # Exit if ESC pressed
+    k = cv2.waitKey(10) & 0xff
+    if k == 27:
+        break
+    """
+    # Process video and track objects
+while video.isOpened():
+  success, frame = video.read()
+  if not success:
+    break
+  
+  # get updated location of objects in subsequent frames
+  success, boxes = multiTracker.update(frame)
+
+  # draw tracked objects
+  for i, newbox in enumerate(boxes):
+    p1 = (int(newbox[0]), int(newbox[1]))
+    p2 = (int(newbox[0] + newbox[2]), int(newbox[1] + newbox[3]))
+    cv2.rectangle(frame, p1, p2, colors[i], 2, 1)
+
+  # show frame
+  cv2.imshow('MultiTracker', frame)
+  
+
+  # quit on ESC button
+  if cv2.waitKey(1) & 0xFF == 27:  # Esc pressed
+    break
